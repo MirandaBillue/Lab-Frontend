74cbfc2ba81a46599e99f259186ea772e261fcde
Miranda Billue <110904846+MirandaBillue@users.noreply.github.com>
2022-09-27 16:23:29 -0400

    Frontend Complete

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(props) {
+    return (
+      <div className="footer">
+    <span><strong>Copyright © Titans 2022</strong></span>
+    <br />
+    <h4>Created by Miranda Billue</h4>
+    </div>
+     )
+  }
+  
+  export default Footer;
